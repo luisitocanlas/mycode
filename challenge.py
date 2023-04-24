1ef7ae49fa00954cec4fb64e16ad47839bba9a4b
luisitocanlas <luisitocanlas@gmail.com>
2023-04-24 22:13:03 +0000

    Lab 18 Challenge

---
+++ challenge.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def main():
+    name = input("What is your name? ")
+    day = input("What day of the week is it? ")
+
+    print(f"Hello, {name}! Happy {day}!")
+
+main()
