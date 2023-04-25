aff6d66b6ae6e650f9b39e8d45958550fa4fcf74
luisitocanlas <luisitocanlas@gmail.com>
2023-04-25 15:13:01 +0000

    day2 updates

---
+++ bustedcode.py
@@ -0,0 +1,8 @@
+#!/usr/env python
+
+def calculator():
+   num1= input("Enter your first number: ")
+   num2= input("Enter your second number: ")
+   print(f"{num1} + {num2} = {int(num1) + int(num2)}")
+
+calculator()
