fb0917f6de8d1f1fc69f077eafd48b4b452a5036
luisitocanlas <luisitocanlas@gmail.com>
2023-04-25 21:16:05 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   List - making selections from complex lists"""
+
+def main():
+
+    animalList = ["cat", "dog", "cow", "fox", "ant"]
+
+    print(animalList)
+
+main()
+
